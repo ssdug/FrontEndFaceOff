dc77bffb3b39c16dfe38da342e81380bd0cd5e56
Chris Fuess <cfuess@LIVE.COM>
2018-02-11 14:01:44 -0800

    Made background black, added js-cookie to packages, added Video component to home page controled by toggle and cookie state.

--- norris-vue/src/App.vue
+++ norris-vue/src/App.vue
@@ -11,12 +11,20 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    margin: 0;
+    border: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    padding-top: 60px;
+    color: #fff;
+    background-color: #000;
+    height: 100%;
+  }
 </style>
--- norris-vue/src/components/Home.vue
+++ norris-vue/src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <coolVideo></coolVideo>
     <h2>{{msg}}</h2>
     <img src="../assets/Chuck-Norris-HD.jpg" style="width: 400px;">
     <rjoke></rjoke>
@@ -12,34 +13,42 @@
 <script>
 import Categories from './Categories'
 import RandomJoke from './Joke'
+import Video from './Video'
+
 export default {
   name: 'Home',
   components: {
     Cat: Categories,
-    rjoke: RandomJoke
+    rjoke: RandomJoke,
+    coolVideo: Video
   },
+
   data () {
     return {
       msg: 'Enlightenment! Compliments of Chuck Norris.'
     }
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
