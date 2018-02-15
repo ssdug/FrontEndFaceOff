bf3caf24a82b627b307792be85d0daffd97b3763
Chris Fuess <cfuess@LIVE.COM>
2018-02-14 17:34:10 -0800

    Forgot these

--- norris-vue/src/components/Categories.vue
+++ norris-vue/src/components/Categories.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="categories">
+  <div>
     <h2>Joke Categories</h2>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg" v-for="cat in categories">
-          <toggle-button id="categorySwitch"
-            v-model="category"
-            :width="250" :height="40"
-            :color="{checked: 'green', unchecked: 'gray'}"
-            :labels="{checked: cat, unchecked: cat}"
-            />
-        </div>
+    <div class="container categories">
+      <div v-for="(cat, i) in categories">
+        <toggle-button class="categorySwitch"
+              v-model="cat.Include"
+              :key="i"
+              :width="switchWidth" :height="switchHeight"
+              :color="{checked: 'green', unchecked: 'gray'}"
+              :labels="{checked: cat.Name, unchecked: cat.Name}"
+              />
       </div>
     </div>
   </div>
@@ -18,26 +17,67 @@
 
 <script>
 import axios from 'axios'
+import Category from '../model/category'
 
 export default {
   name: 'Categories',
+
+  props: {
+    testing: true
+  },
+
   data () {
     return {
-      categories: ['categories'],
-      msg: 'test this string'
+      categories: {},
+      msg: 'test this string',
+      switchWidth: 200,
+      switchHeight: 40
     }
   },
+
+  watch: {
+    categories: {  // watch the categories for changes; call getSelectedCategories to notify the parent  
+      handler: function (val, oldVal) {
+        this.getSelectedCategories()
+      },
+      deep: true // when watching an object properties use the handler and go deep
+    }
+  },
+
   methods: {
     getCategories () {
       let vm = this
+      let data = []
+      console.log(`testing: ${vm.testing}`)
+      if (vm.testing) {
+        data = vm.getMockCategories()
+      } else {
+        data = vm.getAxiosCategories()
+      }
+      vm.categories = data.map(function (obj) { return new Category(obj, false) })
+    },
+    getMockCategories () {
+      console.log('getMockCategories')
+      return ['dev', 'movie', 'food', 'celebrity', 'science', 'sport', 'political', 'religion', 'animal', 'history', 'music', 'travel', 'career', 'money', 'explicit', 'fashion']
+    },
+    getAxiosCategories () {
+      console.log('getAxiosCategories')
       axios.get('https://api.chucknorris.io/jokes/categories')
         .then(function (response) {
-          console.log(response)
-          vm.categories = response.data
+          // console.log(response)
+          return response.data
         })
         .catch(function (error) {
           console.log(error)
         })
+    },
+    getSelectedCategories () {
+      let vm = this
+      let selectedCategories = vm.categories.filter(function (el) {
+        return el.Include === true
+      })
+      var categoryStrings = selectedCategories.map(a => a.Name)
+      this.$emit('selectedCategories-changed', categoryStrings) // this will notify the parent that the object has changed
     }
   },
 
@@ -52,22 +92,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 5px 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-.vue-js-switch {
-  font-size: 2em;
-}
+  .categories {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .vue-js-switch {
+    font-size: 20px;
+  }
 </style>
--- norris-vue/src/components/joke.vue
+++ norris-vue/src/components/joke.vue
@@ -6,13 +6,18 @@
           <h2>{{ msg }}</h2>
         </div>
       </div>
+      <div class="row">
+        <div class="offset-md-10 col-md-2">
+          {{ randomCategory }}
+        </div>
+      </div>
     </div>
-    <div>
+    <div class="switchContainer">
       <toggle-button id="jokeSwitch"
           v-model="randomJokeEnabled"
           :width="250" :height="40"
           :color="{checked: 'green', unchecked: 'gray'}"
-          :labels="{checked: 'MORE NORRIS JOKES!!!', unchecked: 'I can not take anymore'}"
+          :labels="{checked: 'NORRIS JOKES ON!!!', unchecked: 'I can not take anymore'}"
           />
     </div>
   </div>
@@ -23,18 +28,85 @@ import axios from 'axios'
 
 export default {
   name: 'Joke',
+
+  props: { // passed into component from parent using attributes
+    selectedCategories: {
+      type: Array,
+      default: () => ([])
+    },
+    testing: true // if true it will use mock service to enable offline development
+  },
+
   data () {
     return {
-      msg: '',
-      randomJokeEnabled: true
+      msg: 'Pick a Category below',
+      randomJokeEnabled: true, // bound to the button
+      randomCategory: '' // holds the current category to pass to the service
+    }
+  },
+
+  watch: { // watching for change so when toggled and on it will get the next joke
+    randomJokeEnabled: {
+      handler: function (val, oldVal) {
+        if (val) {
+          this.getJoke()
+        }
+      }
     }
   },
 
   methods: {
+    jokeOn () { // method will determine if call to api should be made
+      let vm = this
+      if (vm.randomJokeEnabled) { // toggle must be on
+        // if (vm.selectedCategories.length > 0) { // a category must be selected
+        return true
+        // } else {
+        //   vm.msg = 'Pick a Category below'
+        // }
+      }
+      // return false
+    },
+    getRandomCategory () {
+      let vm = this
+      if (vm.selectedCategories.length > 0) {
+        var randomNumber = Math.floor(Math.random() * vm.selectedCategories.length)
+        vm.randomCategory = vm.selectedCategories[randomNumber]
+      } else {
+        vm.randomCategory = ''
+      }
+    },
     getJoke () {
       let vm = this
-      axios.get('https://api.chucknorris.io/jokes/random')
+      // vm.getRandomCategory()  // get a random category from the selected items
+      if (vm.testing) {
+        vm.getMockJoke()
+      } else {
+        vm.getAxiosJoke()
+      }
+    },
+    getMockJoke () {
+      let vm = this
+      console.log('getMockJoke')
+      let jokes = [
+        'When Chuck Norris was born he round house kicked the Doctor in the face Slow mo\u0027 3 times for slapping his ass.',
+        'Chuck Norris was once bitten by a rattlesnake in the desert, and after 3 days of pain and agony, the snake died.',
+        'Chuck Norris likes his coffee half and half: half coffee grounds, half wood-grain alcohol.',
+        'In the beginning there was nothing...then Chuck Norris Roundhouse kicked that nothing in the face and said \u0022Get a job\u0022. That is the story of the universe.'
+      ]
+      if (jokes.length > 0) { // does not care about cateory will just get a joke for display purposes
+        var randomNumber = Math.floor(Math.random() * jokes.length)
+        vm.msg = jokes[randomNumber]
+      } else {
+        vm.msg = 'No Joke'
+      }
+    },
+    getAxiosJoke () {
+      console.log('getAxiosJoke')
+      let vm = this
+      axios.get(`https://api.chucknorris.io/jokes/random?category=${vm.randomCategory}`)
         .then(function (response) {
+          // console.log(response)
           vm.msg = response.data.value
         })
         .catch(function (error) {
@@ -49,7 +121,7 @@ export default {
     // Requirement #4
     // As a classic javascript developer, I want to make a random joke appear every so many (5?) seconds on the website.
     setInterval(function () {
-      if (this.randomJokeEnabled) {
+      if (this.jokeOn()) {
         this.getJoke()
       }
     }.bind(this), 10000)
@@ -69,6 +141,10 @@ export default {
   .joke {
     padding: 15px;
     background-color: #222;
+  }
+
+  .switchContainer {
+    padding-top: 10px;
   }
 
   a {
